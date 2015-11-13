c55e758945abfe25cf9bc170668a861b4fa97ca8
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-11-13 05:44:23 +0100

    Moved function

--- chrony/core.py
+++ chrony/core.py
@@ -2,3 +2,6 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+
+def compute_category_index(categories):
+    return {category: index + 1 for index, category in enumerate(sorted(set(categories)))}
